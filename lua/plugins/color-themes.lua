01486eb61e3c16e48c31d6d7f8a068fcb054bf9b
jsp <zinsani@gmail.com>
2023-03-24 09:43:17 +0900

    Set colorscheme with everforest

---
+++ lua/plugins/color-themes.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "morhetz/gruvbox",
+  },
+  {
+    "edeneast/nightfox.nvim",
+  },
+  {
+    "sainnhe/everforest",
+    config = function()
+      vim.cmd("colorscheme everforest")
+    end,
+  },
+}
