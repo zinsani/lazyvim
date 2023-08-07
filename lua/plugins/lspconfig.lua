62ff423589d500135fa4d68e13985e659b595156
jsp <zinsani@gmail.com>
2023-08-07 09:31:43 +0900

    Update plugins, Fix typescript treesitter error

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -25,4 +25,7 @@ return {
       },
     },
   },
+  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+  { import = "lazyvim.plugins.extras.lang.typescript" },
 }
